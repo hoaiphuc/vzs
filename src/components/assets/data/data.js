41903e50a556ff189b7ac2ec87df83561e2aa69a
Hoang <hoangdmse150785@fpt.edu.vn>
2023-03-24 14:19:16 +0700

    update data

--- src/components/assets/data/data.js
+++ src/components/assets/data/data.js
@@ -34,61 +34,63 @@ export const category = [
     title: "face & skin care",
     cover: "../images/category/ca1.png",
   },
+  // {
+  //   id: 2,
+  //   title: "baby care",
+  //   cover: "./images/category/ca2.png",
+  // },
+  // {
+  //   id: 3,
+  //   title: "beauty care",
+  //   cover: "../images/category/ca3.png",
+  // },
   {
     id: 2,
-    title: "baby care",
-    cover: "./images/category/ca2.png",
-  },
-  {
-    id: 3,
-    title: "beauty care",
-    cover: "../images/category/ca3.png",
-  },
-  {
-    id: 4,
     title: "contraceptive",
     cover: "../images/category/ca4.png",
   },
   {
-    id: 5,
+    id: 3,
     title: "feminine hygiene",
     cover: "../images/category/ca5.png",
   },
+  // {
+  //   id: 6,
+  //   title: "health & protein",
+  //   cover: "../images/category/ca6.png",
+  // },
   {
-    id: 6,
-    title: "health & protein",
-    cover: "../images/category/ca6.png",
-  },
-  {
-    id: 7,
+    id: 4,
     title: "health & wellness",
     cover: "../images/category/ca7.png",
   },
   {
-    id: 8,
+    id: 5,
     title: "medicine & first aid",
     cover: "../images/category/ca8.png",
   },
   {
-    id: 9,
+    id: 6,
     title: "oral care",
     cover: "../images/category/ca9.png",
   },
   {
-    id: 10,
+    id: 7,
     title: "pregnacy",
     cover: "../images/category/ca10.png",
   },
   {
-    id: 11,
+    id: 8,
     title: "sexual well-being",
     cover: "../images/category/ca11.png",
   },
   {
-    id: 12,
+    id: 9,
     title: "Household",
     cover: "../images/category/ca12.jpg",
   },
+
+  
 ]
 export const product = [
   {
